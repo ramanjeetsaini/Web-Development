f2df708bcde0260b5ef6f2b9c94b3e17f08324c6
Ramanjeet Saini <rj.s.saini@gmail.com>
2020-06-30 19:37:21 +0530

    commiting remaining changes

--- ebay_dropdown/dropdown_server.js
+++ ebay_dropdown/dropdown_server.js
@@ -1,71 +1,123 @@
-//client side
 const HOST = 'server.com/';
 
-function showSubmenu(){
-    const item = document.getElementsByClassName("menu__sub")[0];
-    item.style.display = "block";
-
-}
-
-function hideSubmenu(){
-    const item = document.getElementsByClassName("menu__sub")[0];
-    item.style.display ="none";
-}
-
-let active = null;
-function onMenuItemEnter(menuItem){
-    if (active){
-        active.classList.remove("main__menu__item-active");
+function populateCategories(category) {
+  const activeMenuItemName = activeMenuItem.children[0].innerHTML;
+  api.get(HOST + 'categories', {category, menuItem: activeMenuItemName}, function(categories) {
+    let newCategories = '';
+    for (const category of categories) {
+      const categoryElement = `
+        <li class="menu__sub__categories__item">
+          <a href="#" class="menu__sub__categories__item__link">${category}</a>
+        </li>
+      `;
+      newCategories += categoryElement;
     }
-    active = menuItem;
-    menuItem.classList.add("main__menu__item--active");
-    showSubmenu();
+    const categoriesElement = document.getElementsByClassName(`menu__sub__categories__items--${category}`)[0];
+    categoriesElement.innerHTML = newCategories;
+  });
 }
 
-const menuElement = document.getElementsByClassName("menu__main__item");
-for(const menuItem of menuElement){
-    menuItem.onmouseenter = () => onMenuItemEnter(menuItem)
+function showSubmenu() {
+  const submenu = document.getElementsByClassName("menu__sub")[0];
+  submenu.style.display = "block";
+
+  populateCategories('top');
+  populateCategories('additional');
+}
+
+function hideSubmenu() {
+  const submenu = document.getElementsByClassName("menu__sub")[0];
+  submenu.style.display = "none";
+}
+
+let activeMenuItem = null;
+
+function onMenuItemMouseEnter(item) {
+  if (activeMenuItem) {
+    activeMenuItem.classList.remove("menu__main__item--active");
+  }
+  activeMenuItem = item;
+  item.classList.add("menu__main__item--active");
+  showSubmenu();
+}
+
+const menuItems = document.getElementsByClassName("menu__main__item");
+for (const menuItem of menuItems) {
+  menuItem.onmouseenter = () => onMenuItemMouseEnter(menuItem)
 }
 
 const menu = document.getElementsByClassName("menu")[0];
-menu.onmouseleave = hideSubmenu
+menu.onmouseleave = hideSubmenu;
 
+// Server
 
-
-
-
-
-
-//server 
-function getCategories(data){
-    switch(data.category){
-        case "top":
-            return [
-                "top Apple",
-                "top Banana",
-                "top Mangoes",
-                "top Pears",
-            ]
-        case "additional":
-            return [
-                "Add Apple",
-                "Add Banana",
-                "Add Mangoes",
-                "Add Pears",
-            ]
-        default:
-            return []
-    }   
+function getCategories(data) {
+  if (data.category == 'top') {
+    if (data.menuItem == 'Motors') {
+      return [
+        'Car',
+        'Motorcycle',
+        'Plane',
+        'Trucks',
+        'Wheels'
+      ];
+    }
+    if (data.menuItem == 'Fashion') {
+      return [
+        'Women\'s tops',
+        'Men\'s tops',
+        'Jeans',
+        'Hats'
+      ];
+    }
+    return [
+      'Server apple',
+      'Server banana',
+      'Server pear',
+      'Server orange'
+    ];
+  }
+  if (data.category == 'additional') {
+    if (data.menuItem == 'Motors') {
+      return [
+        'Tires',
+        'Windshields',
+        'Ski racks',
+        'Doors',
+        'Windows'
+      ];
+    }
+    if (data.menuItem == 'Fashion') {
+      return [
+        'On sale',
+        'Red stuff',
+        'Gucci',
+        'New Arrivals'
+      ];
+    }
+    return [
+      'Server square',
+      'Server circle',
+      'Server oval',
+      'Server diamond'
+    ];
+  }
+  return [];
 }
 
-// API library
-const endpoint = {
-    "/categories": getCategories
+const endpoints = {
+  "/categories": {
+    "get": getCategories
+  }
 }
 
-function getFunction(url,data,callback){
-    const domain = url.substring(0,url.indexof("/"));
-    const endpoint = url.substring(url.indexof("/"),url.length);
+function getFunction(url, data, callback) {
+  const domain = url.substring(0, url.indexOf("/"));
+  const endpoint = url.substring(url.indexOf("/"), url.length);
 
-    callback(endpoints[endpoint]["get"](data));
+  callback(endpoints[endpoint]["get"](data));
 }
+
+const api = {
+  get: getFunction
+};
